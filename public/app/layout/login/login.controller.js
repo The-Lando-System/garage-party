246789fc98d039df808ef0a0f2a911bd0d237360
Matt Voget <matt.voget@gmail.com>
2016-05-22 16:18:49 -0600

    added loading to logins

--- public/app/layout/login/login.controller.js
+++ public/app/layout/login/login.controller.js
@@ -12,22 +12,29 @@ function LoginController($http,$window,$location,jwtHelper,AuthService) {
 	loginVm.authFail = false;
 	loginVm.login = login;
 	loginVm.hideLoginDialog = hideLoginDialog;
+	loginVm.loading = false;
 
 	function login(formIsValid){
 		if (formIsValid){
+
+			loginVm.loading = true;
+
 			$http.post('/authenticate',loginVm.creds)
 			.success(function(data){
 				if (data.success){
 					AuthService.createSession(data.token);
 					loginVm.userSession = AuthService.startUserSession();
 					hideLoginDialog();
+					loginVm.loading = false;
 				} else {
 					loginVm.authFail = true;
 					loginVm.errorMessage = data.message;
+					loginVm.loading = false;
 				}
 			})
 			.error(function(data){
 				console.log('Error: ' + data);
+				loginVm.loading = false;
 			});
 		}
 	};
